c90c4cb2e55055e1abcfd5ac0721099b883161ac
ouyang <1697006288@qq.com>
2017-12-22 10:36:53 +0800

    update

--- Pods/Target Support Files/Pods-365physicaleducation/Pods-365physicaleducation-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_365physicaleducation : NSObject
-@end
-@implementation PodsDummy_Pods_365physicaleducation
-@end
--- Pods/Target Support Files/Pods-365physicaleducationUITests/Pods-365physicaleducationUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_365physicaleducationUITests : NSObject
-@end
-@implementation PodsDummy_Pods_365physicaleducationUITests
-@end
